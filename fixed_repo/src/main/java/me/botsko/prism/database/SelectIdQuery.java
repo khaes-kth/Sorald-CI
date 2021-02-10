efa2a83fd693ce7b9648badb97831face1cbbe46
Khashayar Etemadi <khaes@kth.se>
2021-02-10 21:07:22 +0000

    "fixed"

---
+++ fixed_repo/src/main/java/me/botsko/prism/database/SelectIdQuery.java
@@ -0,0 +1,16 @@
+package me.botsko.prism.database;
+
+/**
+ * Created for use for the Add5tar MC Minecraft server
+ * Created by benjamincharlton on 5/04/2019.
+ */
+public interface SelectIdQuery extends SelectQuery {
+
+    void setMax();
+
+    void setMin();
+
+    void setMinMax();
+
+    long[] execute();
+}
