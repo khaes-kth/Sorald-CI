367a2eb4e6e4cc487b7dfbe627044d3f14c9709b
Khashayar Etemadi Someoliayi <khaes@kth.se>
2021-03-10 18:04:10 +0100

    new tests added

--- src/test/java/se/kth/castor/ci/githubapi/commits/GithubAPICommitAdapterTest.java
+++ src/test/java/se/kth/castor/ci/githubapi/commits/GithubAPICommitAdapterTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import se.kth.castor.ci.daemons.GithubScanner;
 import se.kth.castor.ci.githubapi.commits.models.SelectedCommit;
+import se.kth.castor.ci.githubapi.repositories.GithubAPIRepoAdapter;
 
 import java.io.IOException;
 import java.util.Calendar;
@@ -23,13 +24,35 @@ public class GithubAPICommitAdapterTest {
     }
 
     @Test
-    public void listCommits_withNullRepoList() throws IOException {
+    public void listCommits_withoutSpecifiedReposList() throws IOException {
         List<SelectedCommit> commits =
                 GithubAPICommitAdapter.getInstance()
-                        .getSelectedCommits(new Date().getTime() - 2L * 60 * 60 * 1000,
-                                new Date().getTime(),
-                                GithubScanner.FetchMode.ALL, null, false);
+                        .getSelectedCommits(new Date().getTime() - 24L * 60 * 60 * 1000,
+                                new Date().getTime(), 10000, GithubAPIRepoAdapter.MAX_STARS,
+                                GithubScanner.FetchMode.ALL, false);
 
         Assert.assertFalse(commits.isEmpty());
+    }
+
+    @Test
+    public void listCommits_withInvalidRepoList() throws IOException {
+        List<SelectedCommit> commits =
+                GithubAPICommitAdapter.getInstance()
+                        .getSelectedCommits(new Date().getTime() - 24L * 60 * 60 * 1000,
+                                new Date().getTime(), GithubScanner.FetchMode.ALL,
+                                new HashSet<String>(List.of("unknown/not_defined")), false);
+
+        Assert.assertTrue(commits.isEmpty());
+    }
+
+    @Test
+    public void listCommits_withValidReposList() throws IOException {
+        List<SelectedCommit> commits =
+                GithubAPICommitAdapter.getInstance()
+                        .getSelectedCommits(0,
+                                new Date().getTime(), GithubScanner.FetchMode.ALL,
+                                new HashSet<String>(List.of("khaes-kth/sorald-ci")), false);
+
+        Assert.assertTrue(commits.size() > 10);
     }
 }
--- src/test/java/se/kth/castor/ci/githubapi/repositories/GithubAPIRepoAdapterTest.java
+++ src/test/java/se/kth/castor/ci/githubapi/repositories/GithubAPIRepoAdapterTest.java
@@ -1,6 +1,7 @@
 package se.kth.castor.ci.githubapi.repositories;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -8,17 +9,29 @@ import java.util.Calendar;
 import java.util.Set;
 
 public class GithubAPIRepoAdapterTest {
+    static long firstDayOf2021;
 
-    @Test
-    public void listJavaRepositories_withStars() throws IOException {
+    @BeforeAll
+    public static void setUp(){
         Calendar cal = Calendar.getInstance();
         cal.set(2021, 1, 1);
 
-        long intervalStart = cal.getTime().getTime();
+        firstDayOf2021 = cal.getTime().getTime();
+    }
 
+    @Test
+    public void listJavaRepositories_withStars() throws IOException {
         Set<String> repos = GithubAPIRepoAdapter.getInstance()
-                .listJavaRepositories(intervalStart, 1000, GithubAPIRepoAdapter.MAX_STARS);
+                .listJavaRepositories(firstDayOf2021, 1000, GithubAPIRepoAdapter.MAX_STARS);
 
         Assert.assertTrue(repos.size() > 5);
+    }
+
+    @Test
+    public void listJavaRepositories_withInvalidStarLimits() throws IOException {
+        Set<String> repos = GithubAPIRepoAdapter.getInstance()
+                .listJavaRepositories(firstDayOf2021, 1000, -1);
+
+        Assert.assertTrue(repos.isEmpty());
     }
 }
